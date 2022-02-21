fe50f14e7f3ca53f5c55ca925c3305e53135d9df
im-sarvar <mohammedsarvar1995@gmail.com>
2022-02-21 08:21:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <h1>E-commerce Project</h1>
+      <button>Normal button</button>
+      <Button>Bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
